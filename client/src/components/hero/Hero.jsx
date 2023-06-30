ac3c710b2c885355b2f38f2072866abcb59a0d16
Software Engineer <devop.jas@gmail.com>
2023-06-30 02:01:14 -1200

    added shop the sale

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <div className="bg-white">
       <div className="hero-wrapper p-10">
         <div className="grid grid-cols-1 md:grid-cols-2 bg-orange-500 rounded-2xl">
-          <div className="grid-item p-5 mt-6 lg:ml-24 ml-12">
+          <div className="grid-item p-5 mt-6 lg:ml-24 ml-6">
             <div>
               <span>
                 <b className=" text-3xl md:text-4xl"> EXTRA 20% OFF</b> <br />
@@ -21,14 +21,14 @@ export const Hero = () => {
                 Shop now
               </button>
             </div>
-            <div className="terms-wrapper mt-10">
+            <div className="terms-wrapper  mt-2 md:mt-10">
               <p className="text-black">
                 <ul className="underline">See terms</ul>
               </p>
             </div>
           </div>
-          <div className="grit-item p-5  lg:ml-24 ml-12">
-            <div className="mt-12">
+          <div className="grit-item p-5  lg:ml-24 ml-6">
+            <div className="mt-2 md:mt-12">
               <span className="text-3xl md:text-4xl">SAVE 20%</span>
             </div>
           </div>
--- client/src/components/products/shop-sale/ShopSale.jsx
+++ client/src/components/products/shop-sale/ShopSale.jsx
@@ -4,6 +4,12 @@ export const ShopSale = () => {
   return (
     <div className="shop-sale-wrapper mx-10">
       <span className="text-black text-2xl font-semibold">SHOP THE SALE!</span>
+      <div className="grid grid-cols-2 md:grid-cols-4 text-black mt-2 text-center">
+        <div className="grid-item bg-green-300">GRID #1</div>
+        <div className="grid-item bg-yellow-300">GRID #2</div>
+        <div className="grid-item bg-black text-orange-300">GRID #3</div>
+        <div className="grid-item bg-violet-600">GRID#4</div>
+      </div>
     </div>
   );
 };
