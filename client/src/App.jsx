ec6ef30f2bff92e9db62b4ac18e767bb3188d8ee
Software Engineer <devop.jas@gmail.com>
2023-06-30 01:08:12 -1200

    Initialize repository

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-center bg-black text-white">
+        This is the App that will set up the Routes.
+      </h1>
+    </div>
+  );
+};
