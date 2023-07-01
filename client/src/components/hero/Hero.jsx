57f7a7e70b0d64f2b8b4bd0c67c946aedf97ce5e
Software Engineer <devop.jas@gmail.com>
2023-06-30 17:24:45 -1200

    Added shopthe sale component

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -4,11 +4,15 @@ export const Hero = () => {
   return (
     <div className="bg-white">
       <div className="hero-wrapper p-10">
-        <div className="grid grid-cols-1 md:grid-cols-2 bg-orange-500 rounded-2xl">
+        <div className="grid grid-cols-1  md:grid-cols-2 bg-orange-300  md:bg-orange-400 rounded-2xl ">
           <div className="grid-item p-5 mt-6 lg:ml-24 ml-6">
             <div>
               <span>
-                <b className=" text-3xl md:text-4xl"> EXTRA 20% OFF</b> <br />
+                <b className=" text-3xl md:text-4xl text-orange-600 md:text-white">
+                  {" "}
+                  EXTRA 20% OFF
+                </b>{" "}
+                <br />
               </span>
             </div>
             <div>
@@ -27,9 +31,12 @@ export const Hero = () => {
               </p>
             </div>
           </div>
-          <div className="grit-item p-5  lg:ml-24 ml-6">
+          <div
+            className="grit-item p-16  lg:ml-24 ml-6 bg-no-repeat bg-center object-fit"
+            style={{ backgroundImage: `url('./products/bg-image.png')` }}
+          >
             <div className="mt-2 md:mt-12">
-              <span className="text-3xl md:text-4xl font-bold">SAVE 20%</span>
+              <span className="text-3xl md:text-5xl font-bold text-black"></span>
             </div>
           </div>
         </div>
--- client/src/components/products/shop-sale/ShopSale.jsx
+++ client/src/components/products/shop-sale/ShopSale.jsx
@@ -2,13 +2,31 @@ import React from "react";
 
 export const ShopSale = () => {
   return (
-    <div className="shop-sale-wrapper mx-10">
-      <span className="text-black text-2xl font-semibold">SHOP THE SALE!</span>
-      <div className="grid grid-cols-2 md:grid-cols-4 text-black mt-2 text-center">
-        <div className="grid-item bg-green-300">GRID #1</div>
-        <div className="grid-item bg-yellow-300">GRID #2</div>
-        <div className="grid-item bg-black text-orange-300">GRID #3</div>
-        <div className="grid-item bg-violet-600">GRID#4</div>
+    <div className="shop-sale-wrapper mx-10 ">
+      <span className="text-orange-500 text-2xl lg:text-3xl font-bold ml-2">
+        SHOP THE SALE!
+      </span>
+      <div className="grid grid-cols-2 md:grid-cols-4 text-black mt-2 gap-2 ">
+        <div className="grid-item">
+          <div className="grid place-items-center bg-gray-300 rounded-lg h-42 shadow-2xl">
+            <img src="./products/nestle.png" alt="" className="" />
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid place-items-center bg-gray-300 rounded-lg h-42 shadow-2xl ">
+            <img src="./products/feed.png" alt="" />
+          </div>
+        </div>
+        <div className="grid-item">
+          <div className="grid place-items-center bg-gray-300 rounded-lg h-42 shadow-2xl">
+            <img src="./products/fan.png" alt="" className="" />
+          </div>
+        </div>
+        <div className="grid-item ">
+          <div className="grid place-items-center bg-gray-300 rounded-lg h-42 shadow-2xl">
+            <img src="./products/note.png" alt="" className="" />
+          </div>
+        </div>
       </div>
     </div>
   );
