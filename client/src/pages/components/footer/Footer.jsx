19d0a6f31bdb275e4ca63ab2ced74642e5321970
DanielCabador <dcabador8@gmail.com>
2023-07-04 02:06:28 +0800

    Logo Changes, changed help, Changed footer

--- client/src/pages/components/footer/Footer.jsx
+++ client/src/pages/components/footer/Footer.jsx
@@ -1,35 +1,126 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { AiOutlineBars, AiOutlineClose } from "react-icons/ai";
 
 export const Footer = () => {
   return (
-    <div className="footer-wrapper mt-2 bg-orange-400 text-white">
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mx-8 md:mx-12 lg:mx-24">
-        <div className="grid bg-orange-400 mt-2">
-          <img src="Logo" alt="Logo" />
-          <p>Logo</p>
+    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-white text-gray-500 shadow-inner">
+      <div className="grid gap-10 row-gap-6 mb-8 sm:grid-cols-2 lg:grid-cols-4">
+        <div className="sm:col-span-2">
+          <Link
+            to="/"
+            aria-label="Go home"
+            title="Company"
+            className="inline-flex items-center"
+          >
+            <span className="ml-2 text-xl font-bold tracking-wide uppercase">
+              About us
+            </span>
+          </Link>
+          <div className="mt-6 lg:max-w-sm">
+            <p className="text-sm">
+              We're your go-to online marketplace for seamless buying and selling experiences. We prioritize trust, convenience, and quality to ensure your satisfaction.
+            </p>
+            
+          </div>
         </div>
-        <div className="grid bg-orange-400 mt-2 text-white">
-          <h2 className="text-lg font-semibold mb-3">About us</h2>
-          <p>
-            We're your go-to online marketplace for seamless buying and selling
-            experiences. We prioritize trust, convenience, and quality to ensure
-            your satisfaction.
+        <div className="space-y-2 text-sm">
+          <p className="text-base font-bold tracking-wide">
+            Contacts
+          </p>
+          <div className="flex">
+            <p className="mr-1">Phone:</p>
+            <a
+              href="tel:850-123-5021"
+              aria-label="Our phone"
+              title="Our phone"
+              className="text-gray-500"
+            >
+              123-456-789
+            </a>
+          </div>
+          <div className="flex">
+            <p className="mr-1">Email:</p>
+            <a
+              href="mailto:info@lorem.mail"
+              aria-label="Our email"
+              title="Our email"
+              className="text-gray-500"
+            >
+              Nearbuy@gmail.com
+            </a>
+          </div>
+          <div className="flex">
+            <p className="mr-1">Address:</p>
+            <a
+              href="https://www.google.com/maps"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Our address"
+              title="Our address"
+              className="text-gray-500"
+            >
+              Manila, Philippines
+            </a>
+          </div>
+        </div>
+        <div>
+          <span className="text-base font-bold tracking-wide">
+            Socials
+          </span>
+          <div className="flex items-center mt-1 space-x-3">
+            <a
+              href="/"
+              className="text-gray-500"
+            >
+              <span className="h-5"></span>
+            </a>
+            <a
+              href="/"
+              className="text-gray-500"
+            >
+              <span className="h-6"></span>
+            </a>
+            <a
+              href="/"
+              className="text-gray-500"
+            >
+              <span className="h-5"></span>
+            </a>
+          </div>
+          <p className="mt-4 text-sm">
+            Anything basta description
           </p>
         </div>
-        <div className="grid bg-orange-400 mt-2 text-white">
-          <h2 className="text-lg font-semibold mb-3">Vision</h2>
-          <p>To be the leading online marketplace, connecting buyers and sellers in a seamless and vibrant shopping experience.</p>
-        </div>
-        <div className="grid bg-orange-400 mt-2 text-white">
-          <h2 className="text-lg font-semibold mb-3">Mission</h2>
-          <p>
-            Our mission is to provide a trusted platform that empowers
-            individuals and businesses to buy and sell products easily,
-            fostering growth and entrepreneurship in the digital marketplace.
-          </p>
-        </div>
+      </div>
+      <div className="flex flex-col-reverse justify-between pt-5 pb-10 border-t lg:flex-row">
+        <p className="text-sm">
+          © 2023 NearBuy. All rights reserved.
+        </p>
+        <ul className="flex flex-col mb-3 space-y-2 lg:mb-0 sm:space-y-0 sm:space-x-5 sm:flex-row">
+          <li>
+            <a
+              href="/"
+              className="text-sm"
+            >
+              F.A.Q
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="text-sm"
+            >
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              className="text-sm"
+            >
+              Terms &amp; Conditions
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
