f6fb3c28b1625b3714dd90ef0327842e965d2f16
Software Engineer <devop.jas@gmail.com>
2023-07-02 12:29:07 -1200

    finished contacks

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home }from './Home'
-import { Service }from './Service';
-import { Contact }from './Contact';
-import { NoPage }from './NoPage';
-import { RootLayout }from './RootLayout';
-import { Store } from './Store';
-import { Help } from './Help';
- 
-export const Index = ()=> {
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<RootLayout />}>
-                    <Route index element={<Home />}/>
-                    <Route path="/help" element={<Help />} />
-                    <Route path="/service" element={<Service />} />
-                    <Route path="/store" element={<Store />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./Home";
+import { Service } from "./Service";
+import { Contact } from "./Contact";
+import { NoPage } from "./NoPage";
+import { RootLayout } from "./RootLayout";
+import { Store } from "./Store";
+import { Help } from "./Help";
+import { SignUp } from "./SignUp";
+
+export const Index = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Index;
