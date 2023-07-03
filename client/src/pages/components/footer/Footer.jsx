3b83d004d3403f730072f2cc40d63d36336cfa41
Software Engineer <devop.jas@gmail.com>
2023-07-02 15:03:28 -1200

    Layout for footer

--- client/src/pages/components/footer/Footer.jsx
+++ client/src/pages/components/footer/Footer.jsx
@@ -4,14 +4,12 @@ import { AiOutlineBars, AiOutlineClose } from "react-icons/ai";
 
 export const Footer = () => {
   return (
-    <div className="bg-orange-400 text-white text-center py-6 mt-6 ">
-      <div className="container mx-auto p-2">
-        <p className=" text-xl font-semibold">
-          © {new Date().getFullYear()} NearBuy. All Rights Reserved.
-        </p>
-        <p className="text-sm mt-2 font-medium">
-          Designed and developed by NearBuy.
-        </p>
+    <div className="footer-wrapper mt-3">
+      <div className="grid grid-cols-2 md:grid-cols-4 mx-8 md:mx-12 lg:mx-24">
+        <div className="grid bg-red-400">GRID-1</div>
+        <div className="grid bg-blue-400">GRID-2</div>
+        <div className="grid bg-green-500">GRID-3</div>
+        <div className="grid bg-violet-500">GRID-4</div>
       </div>
     </div>
   );
